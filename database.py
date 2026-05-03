1b8ba85e7a5f2bdc4a75089cd6051c6036fb20e4
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-05-03 09:06:33 +0530

    database.py

--- database.py
+++ database.py
@@ -11,6 +11,29 @@ db = client.rename_bot
 
 users = db.users
 
+# ------------------------- #
+
+async def get_premium_status(uid):
+    user = await users.find_one({"_id": uid})
+    if not user:
+        return False
+
+    if not user.get("premium"):
+        return False
+
+    expiry = user.get("premium_expiry", 0)
+
+    if expiry and time.time() > expiry:
+        await users.update_one(
+            {"_id": uid},
+            {"$set": {"premium": False, "premium_expiry": 0}}
+        )
+        return False
+
+    return True
+    
+# ------------------------- #
+
 async def get_user(uid):
     return await users.find_one({"_id": uid})
 
@@ -20,6 +43,8 @@ async def set_user(uid, data):
         {"$set": data},
         upsert=True
     )
+
+# ------------------------- #
 
 async def add_user(uid):
     await users.update_one(
@@ -31,22 +56,29 @@ async def add_user(uid):
                 "caption": "",
                 "thumb": "",
                 "premium": False,
+                "premium_expiry": 0,
                 "banned": False
             }
         },
         upsert=True
     )
+# ------------------------- #
 
 async def is_banned(uid):
     user = await get_user(uid)
     return user.get("banned", False) if user else False
 
+# ------------------------- #
+
 async def is_premium(uid):
     user = await get_user(uid)
     return user.get("premium", False) if user else False
 
+# ------------------------- #
+
 async def get_all_users():
     return users.find({})
+    
 # ------------------------- #
 # Don't Remove Credit 
 # Ask Doubt @AU_Bot_Discussion 
